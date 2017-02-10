0e0ef975fa2762f6975ca491abe1b7f23b662d8a
aqutw <aquarian.ex+github@gmail.com>
2017-02-10 11:14:19 +0800

    Again demo1

--- demo1/gulpfile.js
+++ demo1/gulpfile.js
@@ -4,13 +4,13 @@ var less = require('gulp-less');
 var minifyCSS = require('gulp-csso');
 
 gulp.task('html', function(){
-  return gulp.src('client/templates/*.pug')
+  return gulp.src('app/*.pug')
     .pipe(pug())
     .pipe(gulp.dest('build/html'))
 });
 
 gulp.task('css', function(){
-  return gulp.src('client/templates/*.less')
+  return gulp.src('app/*.less')
     .pipe(less())
     .pipe(minifyCSS())
     .pipe(gulp.dest('build/css'))
